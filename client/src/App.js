9659443826f19e1ef323791ecb8173952219740e
Desiree Lerma <desi.lerma96@yahoo.com>
2023-03-17 15:22:14 -0500

    Removes unused/unneeded CRA files.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -15,7 +14,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
           <h3>{message}</h3>
       </header>
     </div>
