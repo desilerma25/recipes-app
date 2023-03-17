c5243909fc055a1321da6f97809863e7133b9398
Desiree Lerma <desi.lerma96@yahoo.com>
2023-03-16 22:18:41 -0500

    Adds in a simple call to get server stood up.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/message")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          <h2> Hello World!</h2>
+          <h3>{message}</h3>
       </header>
     </div>
   );
