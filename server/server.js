1cac1c1ea333c3b0f12deadae04b723dcd646d66
Desiree Lerma <desi.lerma96@yahoo.com>
2023-03-20 23:50:41 -0500

    adds port variable and updates dotenv config. Adds node and npm versions to package.json.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const config = require('dotenv').config()
 const SPOONACULAR_API = process.env.SPOONACULAR_API
 
 const app = express();
+const PORT = process.env.PORT || 8000;
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +17,6 @@ app.get('/message', (req, res) => {
     res.json({ message: "Hello, this is the server!"})
 });
 
-app.listen(8000, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 8000')
 });
