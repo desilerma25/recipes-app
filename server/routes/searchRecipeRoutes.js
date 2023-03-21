21bf415453dccf4c2c91459ceac456a938ccadbb
Desiree Lerma <desi.lerma96@yahoo.com>
2023-03-21 03:59:15 -0500

    adds calls for cuisines, search for recipes by cuisines, recipe info.

---
+++ server/routes/searchRecipeRoutes.js
@@ -0,0 +1,56 @@
+require('dotenv').config();
+const SPOONACULAR_API_SECRET = process.env.SPOONACULAR_API
+const baseUrl = 'https://api.spoonacular.com'
+
+module.exports = (app) => {
+    app.get('/cuisines', (req, res) => {
+        res.send([ 
+            "african",
+            "american",
+            "british",
+            "cajun",
+            "caribbean",
+            "chinese",
+            "eastern-european",
+            "european",
+            "french",
+            "german",
+            "greek",
+            "indian",
+            "irish",
+            "italian",
+            "japanese",
+            "jewish",
+            "korean",
+            "latin-american",
+            "mediterranean",
+            "mexican",
+            "middle-eastern",
+            "nordic",
+            "southern",
+            "spanish",
+            "thai",
+            "vietnamese"
+        ])
+    });
+
+    app.get('/:cuisine/recipes', (req, res) => {
+        fetch(baseUrl + `/recipes/complexSearch?apiKey=${SPOONACULAR_API_SECRET}&cuisine=${req.params.cuisine}`)
+        .then((response) => response.json())
+        .then((data) => res.send(data))
+        .catch((error) => {
+            console.error('error in execution', error)
+            res.send(error)
+        })
+    });
+
+    app.get('/recipe/:id', (req, res) => {
+        fetch(baseUrl + `/recipes/${req.params.id}/information?apiKey=${SPOONACULAR_API_SECRET}&includeNutrition=false`)
+        .then((response) => response.json())
+        .then((data) => res.send({data}))
+        .catch((error) => {
+            console.error('error in execution', error)
+            res.send(error)
+        })
+    })
+};
--- server/server.js
+++ server/server.js
@@ -1,9 +1,7 @@
-require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
-const SPOONACULAR_API = process.env.SPOONACULAR_API
-
 const app = express();
+require('./routes/searchRecipeRoutes')(app);
 const PORT = process.env.PORT || 8000;
 
 app.use(cors());
@@ -11,10 +9,6 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({ message: "Hello, this is the homepage of the server!"})
-});
-
-app.get('/message', (req, res) => {
-    res.json({ message: "Hello, this is the server!"})
 });
 
 app.listen(PORT, () => {
